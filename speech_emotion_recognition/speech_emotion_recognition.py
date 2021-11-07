6fb77d946dcdf1a95045213e237147bd4c3fb758
tahany777 <tahanymohammed777@hotmail.com>
2021-11-07 15:39:11 +0200

    feat: extracting emotions from the dataset

--- speech_emotion_recognition/speech_emotion_recognition.py
+++ speech_emotion_recognition/speech_emotion_recognition.py
@@ -1,7 +1,9 @@
 import librosa as lb
 import soundfile as sf
 import numpy as np
- 
+import os
+from glob import glob
+from sklearn.model_selection import train_test_split
 
 def extract_features(file_title, mfcc, chroma, mel):
   '''
@@ -32,4 +34,37 @@ def extract_features(file_title, mfcc, chroma, mel):
   except :
     raise FileNotFoundError
 
+emotion_labels = {
+  '01':'neutral',
+  '02':'calm',
+  '03':'happy',
+  '04':'sad',
+  '05':'angry',
+  '06':'fearful',
+  '07':'disgust',
+  '08':'surprised'
+}
+focused_emotion_labels = ['happy', 'sad', 'angry', 'neutral']
+
+def loading_audio_data():
+    """
+    Arguments: None
+    it loads the sound features for each file and the related emotion for each feature
+    return: Arrays
+    """
+    x = []
+    y = []
+    for file in glob('sounds/Actor_*/*.wav'):
+        print(file)
+        file_path = os.path.basename(file)
+        emotion = emotion_labels[file_path.split("-")[2]]
+        if emotion not in focused_emotion_labels:
+            continue
+        feature = extract_features(file, mfcc=True, chroma=True, mel=True)
+        x.append(feature)
+        y.append(emotion)
+    final_dataset = train_test_split(np.array(x), y, test_size=0.1, random_state=9)
+    return final_dataset
+
+X_train, X_test, y_train, y_test = loading_audio_data()
 
